15accebeb4210d75008ff3f152cbc94b0bbeb932
Joyce Lin <123987150+joyc3lin@users.noreply.github.com>
2023-10-01 23:40:23 -0400

    Update create.sql

--- GCP/create.sql
+++ GCP/create.sql
@@ -1,1 +1,32 @@
+CREATE TABLE patients (
+    patient_id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    date_of_birth DATE
+);
 
+CREATE TABLE demographics (
+    demographic_id INT PRIMARY KEY AUTO_INCREMENT,
+    patient_id INT UNIQUE,
+    preferred_pronouns VARCHAR(15),
+    allergies TEXT,
+    languages_spoken TEXT,
+    relationship_status VARCHAR(50),
+    address TEXT,
+    phone_number VARCHAR(15),
+    email VARCHAR(100),
+    FOREIGN KEY (patient_id) REFERENCES patients(patient_id)
+);
+
+CREATE TABLE patient_preferences (
+    patient_preferences_id INT PRIMARY KEY AUTO_INCREMENT,
+    demographic_id INT UNIQUE, 
+    patient_id INT UNIQUE,
+    favorite_dessert VARCHAR(100),
+    favorite_food VARCHAR(100),
+    preferred_cafeteria_meal VARCHAR(100),
+    favorite_shows VARCHAR(100),
+    hobbies VARCHAR(100),
+    FOREIGN KEY (patient_id) REFERENCES patients(patient_id),
+    FOREIGN KEY (demographic_id) REFERENCES demographics(demographic_id)
+);
